799086f46d1190d53baa1181da1a9816b86644af
Muralikrishna Parla <92716125+Muralikrishnaparla@users.noreply.github.com>
2024-02-18 14:01:06 +0530

    Update snake_n_ladder.c

--- Snake-and-ladder-game-in-C-master/snake_n_ladder.c
+++ Snake-and-ladder-game-in-C-master/snake_n_ladder.c
@@ -15,7 +15,7 @@ void displaychart(int curp,char player[4])
 		
 		if(curp==100)
 		{
-			printf("*****Congratulations*****\n\n\nPlayer %s wins\n",player);
+			printf("Congratulations\n\n\nPlayer %s wins\n",player);
 			scanf("%*s");
 			exit(0);
 		}
@@ -66,7 +66,7 @@ void main()
 	int i,dice,cur_pos1=0,cur_pos2=0;
 	char ch;
 	while(1)
-	{	printf("		** SNAKE AND LADDER GAME** \n		Coded By Akash Mahesh Ganjal\n");
+	{	printf(" SNAKE AND LADDER GAME \n\n");
 		printf("Snakes:- 25 to 9,\t 65 to 40,\t 99 to  1.\nLadder:- 13 to 42,\t 60 to 83,\t 70 to 93.\n");
 		printf("Choose your option\n");
 		printf("1. Player 1 plays\n");
@@ -77,36 +77,36 @@ void main()
 		switch(ch)
 		{
 			
-			case '1':dice=rd();
+			case '1':dice=read();
 			system("cls");
 					printf("\t\t\t\tDice = %d\n\n",dice);
 					if(dice==6)
-					printf("Dice=6: You have earned a chance to play one more time.\n");
+					printf("Dice=6: now u can rool one more time.\n");
 					cur_pos1=dice+cur_pos1;
 					if(cur_pos1<101){
 						if(cur_pos1==99)
 						{
-						displaychart(1,"$P1$");//snake
+						displaychart(1,"$P1$");
 						}
 						if(cur_pos1==65)
 						{
-						displaychart(40,"$P1$");//snake
+						displaychart(40,"$P1$");
 						}
 						if(cur_pos1==25)
 						{
-						displaychart(9,"$P1$");//snake
+						displaychart(9,"$P1$");
 						}
 						if(cur_pos1==70)
 						{
-						displaychart(93,"$P1$");//ladder
+						displaychart(93,"$P1$");
 						}
 						if(cur_pos1==60)
 						{
-						displaychart(83,"$P1$");//ladder
+						displaychart(83,"$P1$");
 						}
 						if(cur_pos1==13)
 						{
-						displaychart(42,"$P1$");//ladder
+						displaychart(42,"$P1$");
 						}
 						else{
 							displaychart(cur_pos1,"$P1$");
@@ -121,32 +121,32 @@ void main()
 					printf("Player 2 position is %d\n",cur_pos2);
 			
 				break;
-			case '2':dice=rd();
+			case '2':dice=read();
 			system("cls");
 					printf("\t\t\t\tDice = %d\n\n",dice);
 					cur_pos2=dice+cur_pos2;
 					if(cur_pos2<101){
-						if(cur_pos2==99)	//snake
+						if(cur_pos2==99)	
 						{
 						displaychart(1,"$P2$");
 						}
-						if(cur_pos2==65)	//snake
+						if(cur_pos2==65)	
 						{
 						displaychart(40,"$P2$");
 						}
-						if(cur_pos2==25)	//snake
+						if(cur_pos2==25)	
 						{
 						displaychart(9,"$P2$");
 						}
-						if(cur_pos2==70)	//ladder
+						if(cur_pos2==70)	
 						{
 						displaychart(93,"$P2$");
 						}
-						if(cur_pos2==60)	//ladder
+						if(cur_pos2==60)	
 						{
 						displaychart(83,"$P2$");
 						}
-						if(cur_pos2==13) 	//ladder
+						if(cur_pos2==13) 	
 						{
 						displaychart(42,"$P2$");
 						}
